504f10bb0d509a17f56147fbf314326eabc2dcc1
Gustavo Guichard <gustavoguichard@gmail.com>
2018-03-29 20:06:45 -0300

    Add setNow to every record

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,14 +1,15 @@
 // Application hooks that run for every service
 const logger = require('./hooks/logger');
+const { setNow } = require('feathers-hooks-common');
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [ setNow('createdAt', 'updatedAt') ],
+    update: [ setNow('updatedAt') ],
+    patch: [ setNow('updatedAt') ],
     remove: []
   },
 
