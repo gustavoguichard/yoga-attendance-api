e46ab261c838f20dcd57b7b7375bf26876deca00
Gustavo Guichard <gustavoguichard@gmail.com>
2018-05-29 00:28:34 -0300

    Test populateClassroom

--- src/models/frequency.model.js
+++ src/models/frequency.model.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
   const { Schema, SchemaTypes } = mongooseClient
   const frequency = new Schema({
-    practitionerId: { type: SchemaTypes.ObjectId, ref: 'practitioners' },
+    practitionerId: { type: SchemaTypes.ObjectId, required: true, ref: 'practitioners' },
     teacher: { type: Boolean, required: true, default: false },
     classId: { type: SchemaTypes.ObjectId, ref: 'classrooms' },
     createdAt: { type: Date, default: Date.now },
--- test/services/frequency.test.js
+++ test/services/frequency.test.js
@@ -1,10 +1,28 @@
 const assert = require('assert')
 const app = require('../../src/app')
 
+const service = app.service('frequency')
 describe('\'frequency\' service', () => {
+  let frequency, classroom, practitioner
+
   it('registered the service', () => {
-    const service = app.service('frequency')
 
     assert.ok(service, 'Registered the service')
+  })
+
+  before(async () => {
+    classroom = await app.service('classrooms').create({ title: 'Class', tuition: 0 })
+    practitioner = await app.service('practitioners').create({ fullName: 'Test', accessCode: 'foo' })
+    frequency = await service.create({ classId: classroom._id, practitionerId: practitioner._id })
+  })
+
+  describe('populate hooks', async () => {
+    it('populates classrooms', async () => {
+      const result = await service.get(frequency._id)
+      const result2 = await service.get(frequency._id, { populateClassroom: true })
+
+      assert.ok(!result.classroom)
+      assert.equal(result2.classroom.title, 'Class')
+    })
   })
 })
