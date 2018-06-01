fbd9a0e40dc00c9f36c942cf27d6954d4b2b80de
Gustavo Guichard <gustavoguichard@gmail.com>
2018-05-31 22:00:48 -0300

    Change birthdate time to avoid timezone conflicts

--- src/hooks/normalize-data.js
+++ src/hooks/normalize-data.js
@@ -7,8 +7,7 @@ module.exports = function (serviceName) {
       const { data } = hook
 
       if(data.birthdate && !(data.birthdate instanceof Date)) {
-        const date = moment(data.birthdate, 'DD/MM/YYYY')
-        console.log(date.format('DD/MM/YYYY'))
+        const date = moment(data.birthdate, 'DD/MM/YYYY').add(6, 'hour')
         data.birthdate = date.isValid() ? date._d : null
       }
       return hook
