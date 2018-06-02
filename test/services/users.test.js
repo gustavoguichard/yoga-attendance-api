2743768432a679b48e35291e68ed2f2e58d3181e
Gustavo Guichard <gustavoguichard@gmail.com>
2018-06-02 16:18:02 -0300

    100% coverage

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -1,26 +1,39 @@
 const assert = require('assert')
+const { toString } = require('lodash')
 const app = require('../../src/app')
 const fx = require('../fixtures')
 
 const service = app.service('users')
 describe('\'users\' service', () => {
+  beforeEach(async () => {
+    await service.remove(null)
+    await app.service('practitioners').remove(null)
+  })
+
   it('registered the service', () => {
     assert.ok(service, 'Registered the service')
   })
 
-  it('creates a user, encrypts password and adds gravatar', async () => {
+  it('encrypts password', async () => {
     const user = await fx.user()
     assert.ok(user.password !== 'secret')
   })
 
   it('removes password for external requests', async () => {
-    // Setting `provider` indicates an external request
     const params = { provider: 'rest' }
 
     const user = await service.create({
-      email: 'test2@example.com',
+      email: 'test@example.com',
       password: 'secret'
     }, params)
     assert.ok(!user.password)
+  })
+
+  it('retrieves the practitioner along with the user', async () => {
+    const practitioner = await fx.practitioner({ email: 'foo@bar.com' })
+    const result = await fx.user({ email: 'foo@bar.com' })
+    const result2 = (await service.find()).data[0]
+    assert.equal(result.practitioner.fullName, practitioner.fullName)
+    assert.ok(result2.practitioner._id)
   })
 })
