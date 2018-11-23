c68996bb69dcb11877762976fc97c200fb336aa2
Gustavo Guichard <gustavoguichard@gmail.com>
2018-11-23 12:28:41 -0200

    Remove deprecation warnings

--- src/hooks/logger.js
+++ src/hooks/logger.js
@@ -1,26 +1,29 @@
 // A hook that logs service method before, after and error
-const logger = require('winston')
+const winston = require('winston')
 
-module.exports = function () {
-  return function (hook) {
-    let message = `${hook.type}: ${hook.path} - Method: ${hook.method}`
+module.exports = () => hook => {
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [new winston.transports.Console()],
+  })
+  let message = `${hook.type}: ${hook.path} - Method: ${hook.method}`
 
-    if (hook.type === 'error') {
-      message += `: ${hook.error.message}`
-    }
+  if (hook.type === 'error') {
+    message += `: ${hook.error.message}`
+  }
 
-    message = (process.env.NODE_ENV === 'test') ? '' : message
+  message = process.env.NODE_ENV === 'test' ? '' : message
 
-    message && logger.info(message)
-    logger.debug('hook.data', hook.data)
-    logger.debug('hook.params', hook.params)
+  message && logger.info(message)
+  logger.debug('hook.data', hook.data)
+  logger.debug('hook.params', hook.params)
 
-    if (hook.result) {
-      logger.debug('hook.result', hook.result)
-    }
+  if (hook.result) {
+    logger.debug('hook.result', hook.result)
+  }
 
-    if (hook.error) {
-      logger.error(hook.error)
-    }
+  if (hook.error) {
+    logger.error(hook.error)
   }
 }
--- src/mongoose.js
+++ src/mongoose.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
-module.exports = function () {
+function mongooseClient() {
   const app = this
 
-  mongoose.connect(app.get('mongodb'))
+  mongoose.connect(
+    app.get('mongodb'),
+    { useNewUrlParser: true },
+  )
   mongoose.Promise = global.Promise
+  mongoose.set('useCreateIndex', true)
 
   app.set('mongooseClient', mongoose)
 }
+
+module.exports = mongooseClient
